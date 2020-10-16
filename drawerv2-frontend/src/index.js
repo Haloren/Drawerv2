f1bd5ef1d710baec734c02633c1f6c854eaaa9d4
Haloren <mckennakyler@gmail.com>
2020-10-16 03:29:17 -0700

    created fetch to get all players from api

--- drawerv2-frontend/src/index.js
+++ drawerv2-frontend/src/index.js
@@ -3,4 +3,22 @@ const PLAYERS_URL = `${BASE_URL}/players`
 const CARDS_URL = `${BASE_URL}/cards`
 const GAMES_URL = `${BASE_URL}/games`
 
+//FETCH PLAYERS
+const player = document.getElementById("scoreboard")
 
+document.addEventListener("DOMContentLoaded", () => loadPlayers())
+
+const loadPlayers = () => {
+    fetch(PLAYERS_URL)
+    .then(resp => resp.json())
+    // .then(json => {
+    //     console.log(json)
+    // })
+    .then(json => {
+        json.forEach(player => renderPlayer(player))
+    })
+}
+
+const renderPlayer = () => {
+    
+}
