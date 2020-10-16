d8a91bba01e1f55bc99d1808b3b0bfb5b195b1e9
Haloren <mckennakyler@gmail.com>
2020-10-16 02:33:07 -0700

    set up config routes resources

--- drawerv2-backend/config/routes.rb
+++ drawerv2-backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :players
-  resources :games
+  resources :cards, :only => [:index, :create]
+  resources :players, :only => [:index, :show, :create, :destroy]
+  resources :games, :only => [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
