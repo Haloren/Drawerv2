65e89a9bff52c99f6d4ad87b10799a7585f2a1eb
Haloren <mckennakyler@gmail.com>
2020-11-02 21:09:22 -0700

    Players are rendered from db into the scoreboard

--- drawerv2-frontend/src/Player.js
+++ drawerv2-frontend/src/Player.js
@@ -3,9 +3,43 @@ class Player {
 
     constructor(player) {
         this.player = player
-        console.log(this)
+        // console.log(this)
+        this.renderPlayer()
+        this.setEventListeners()
     }
     
+    renderPlayer(){
+        const p = document.createElement("p")
+        const input = document.createElement("input")
+        const button = document.createElement("button")
+    
+        p.setAttribute("class", "player")
+        p.innerHTML = this.player.name
+        input.innerHTML = "0"
+    
+        button.setAttribute("class", "delete-player")
+        button.setAttribute("data-player-id", this.player.id)
+        button.innerHTML = "☠️"
+    
+        input.setAttribute("class", "points")
+        input.setAttribute("type", "number")
+        input.setAttribute("value", "0")
+    
+        // document.getElementById("new-player").addEventListener('submit', createPlayer)
+        // button.addEventListener('click', deletePlayer)
+    
+        scoreboard.appendChild(p)
+        p.appendChild(button)
+        p.appendChild(input)
+    }
+
+    renderInnerHTML(){
+
+    }
+
+    setEventListeners = () => {
+
+    }
 
 
 
@@ -13,33 +47,6 @@ class Player {
         api.getAllPlayers().then(players => players.forEach(player => new Player(player)))
     }
 }
-
-
-// //PLAYERS ON SCOREBOARD: NAME / DELETE(BUTTON) / POINTS(INPUT) 
-// const renderPlayer = (playerList) => {
-//     const p = document.createElement("p")
-//     const input = document.createElement("input")
-//     const button = document.createElement("button")
-
-//     p.setAttribute("class", "player")
-//     p.innerHTML = playerList.name
-//     input.innerHTML = "0"
-
-//     button.setAttribute("class", "delete-player")
-//     button.setAttribute("data-player-id", playerList.id)
-//     button.innerHTML = "☠️"
-
-//     input.setAttribute("class", "points")
-//     input.setAttribute("type", "number")
-//     input.setAttribute("value", "0")
-
-//     document.getElementById("new-player").addEventListener('submit', createPlayer)
-//     button.addEventListener('click', deletePlayer)
-
-//     player.appendChild(p)
-//     p.appendChild(button)
-//     p.appendChild(input)
-// }
 
 // const deletePlayer = (e) => {
 //     e.preventDefault()
