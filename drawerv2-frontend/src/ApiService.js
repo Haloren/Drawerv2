30db3f7f7407ca688acf7a2fbcc222b479fc0d9f
Haloren <mckennakyler@gmail.com>
2020-11-03 00:48:18 -0700

    removed duplicate info notes

--- drawerv2-frontend/src/ApiService.js
+++ drawerv2-frontend/src/ApiService.js
@@ -23,21 +23,3 @@ class ApiService {
 
 
 }
-
-// const createPlayer = (e) => {
-//     e.preventDefault()
-//     const configObj = {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Accept':'application/json'
-//         },
-//         body: JSON.stringify({name: e.target.name.value})
-//     }
-//     fetch(PLAYERS_URL, configObj)
-//     .then(resp => resp.json())
-//     .then(json =>
-//           json.message ? alert(json.message) : renderPlayer(json)
-//     )
-//     e.path[0].reset() //clear form input
-// }
